102879f178af340b2259114853f42c1922edfe57
johnpagtama <pagtamajr@gmail.com>
2022-05-29 18:46:49 -0700

    Added data storage

--- scripts/run.js
+++ scripts/run.js
@@ -1,4 +1,5 @@
 const main = async () => {
+	const [owner, randomPerson] = await hre.ethers.getSigners();
 	const waveContractFactory = await hre.ethers.getContractFactory(
 		`WavePortal`
 	);
@@ -6,6 +7,20 @@ const main = async () => {
 	await waveContract.deployed();
 
 	console.log(`Contract deployed to: ${waveContract.address}`);
+	console.log(`Contract deployed by: ${owner.address}`);
+
+	let waveCount;
+	waveCount = await waveContract.getTotalWaves();
+
+	let waveTxn = await waveContract.wave();
+	await waveTxn.wait();
+
+	waveCount = await waveContract.getTotalWaves();
+
+	waveTxn = await waveContract.connect(randomPerson).wave();
+	await waveTxn.wait();
+
+	waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
